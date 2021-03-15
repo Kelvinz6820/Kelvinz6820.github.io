6a477af9d3c50acd1aa444b402ece2381a78cdbb
Kelvin Zheng <kzheng000@citymail.cuny.edu>
2021-03-14 20:37:34 -0400

    apis

---
+++ personal-website.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let aboutButton = document.querySelector(".about")
+    aboutButton.addEventListener("click", () => {
+        let aboutMeSection = document.querySelector(".aboutMePage")
+        aboutMeSection.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+    })
+
+    let projectsButton = document.querySelector(".projects")
+    projectsButton.addEventListener("click", () => {
+        let projectSection = document.querySelector("#project-header")
+        projectSection.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+    })
+
+    
+    let contactButton = document.querySelector(".contact")
+    contactButton.addEventListener("click", () => {
+        let contactSection = document.querySelector("#contact-header")
+        contactSection.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+    })
+
+    let projectsButton2 = document.querySelector("#projects2")
+    projectsButton2.addEventListener("click", () => {
+        let projectSection2 = document.querySelector("#project-header")
+        projectSection2.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+    })
+
+    let contactButton2 = document.querySelector("#contactMe2")
+    contactButton2.addEventListener("click", () => {
+        let contactSection2 = document.querySelector("#contact-header")
+        contactSection2.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+    })
+
+    let contactButton3 = document.querySelector("#contactMe3")
+    contactButton3.addEventListener("click", () => {
+        let contactSection3 = document.querySelector("#contact-header")
+        contactSection3.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+    })    
+})
